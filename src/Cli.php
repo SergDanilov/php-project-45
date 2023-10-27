d7505643107fa14240c83153aeaaa51c963fa89c
SergDanilov <danilovserg1985s@gmail.com>
2023-10-27 18:25:15 +0300

    fix codesniffer bugs

--- src/Cli.php
+++ src/Cli.php
@@ -1,5 +1,17 @@
 <?php
-
+/**
+ * Greeting and getting to know the game user
+ * php version 8.1.24
+ *
+ * This allows greeting with User and know his name.
+ *
+ * @category  LearnProject
+ * @package   Phpproject
+ * @author    Sergey Danilov <danilovserg1985s@gmail.com>
+ * @copyright 2023 Sergey Danilov
+ * @license   no Licence
+ * @link      https://github.com/SergDanilov/php-project-45/blob/main/src/Cli.php
+ */
 namespace Hexlet\Code\Cli;
 
 use function cli\line;
