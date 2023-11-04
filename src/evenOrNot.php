b319d6222e8929eafc18d844e86d5c48b60a3cad
SergDanilov <danilovserg1985s@gmail.com>
2023-11-04 18:08:37 +0300

    added work logic in the file

--- src/evenOrNot.php
+++ src/evenOrNot.php
@@ -17,10 +17,32 @@ namespace Hexlet\Code\evenOrNot;
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
+line("Welcome to the Brain Game!");
+$name = prompt("May I have your name?");
 line("Hello, %s!", $name);
-line('Answer "yes" if the number is even, otherwise answer "no".');
-line('Question:'.rand());
-$answer = prompt('Your answer:');
-line('Correct');
+line("Answer 'yes' if the number is even, otherwise answer 'no'.");
+$win = 0;
+for ($i = 0; $i < 3;) {
+
+	$number = rand(1, 1000);
+	line('Question: ' . $number);
+	$answer = prompt("Your answer");
+	if ($number % 2 === 0) {
+		$correctAnswer = "yes";
+	} else {
+		$correctAnswer = "no";
+	}
+
+	if ($answer === $correctAnswer) {
+		line("Correct!");
+		$i++;
+		$win += 1;
+	} else {
+		line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+		line("Let's try again, " . $name . "!");
+		break;
+	}
+}
+if ($win == 3) {
+line("Congratulations {$name}!");
+}
