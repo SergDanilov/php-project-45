1db29e5323874f507a2a943c043eda092b80124e
SergDanilov <danilovserg1985s@gmail.com>
2023-11-04 19:09:00 +0300

    correct file with coding standart PSR-12

--- src/evenOrNot.php
+++ src/evenOrNot.php
@@ -23,26 +23,25 @@ line("Hello, %s!", $name);
 line("Answer 'yes' if the number is even, otherwise answer 'no'.");
 $win = 0;
 for ($i = 0; $i < 3;) {
-
-	$number = rand(1, 1000);
-	line('Question: ' . $number);
-	$answer = prompt("Your answer");
-	if ($number % 2 === 0) {
-		$correctAnswer = "yes";
-	} else {
-		$correctAnswer = "no";
-	}
-
-	if ($answer === $correctAnswer) {
-		line("Correct!");
-		$i++;
-		$win += 1;
-	} else {
-		line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
-		line("Let's try again, " . $name . "!");
-		break;
-	}
+    $number = rand(1, 1000);
+    line('Question: ' . $number);
+    $answer = prompt("Your answer");
+    if ($number % 2 === 0) {
+        $correctAnswer = "yes";
+    } else {
+        $correctAnswer = "no";
+    }
+    if ($answer === $correctAnswer) {
+        line("Correct!");
+        $i++;
+        $win += 1;
+    } else {
+        line("'{$answer}' is wrong answer ;(.");
+        line("Correct answer was '{$correctAnswer}'.");
+        line("Let's try again, " . $name . "!");
+        break;
+    }
 }
 if ($win == 3) {
-line("Congratulations {$name}!");
+    line("Congratulations {$name}!");
 }
