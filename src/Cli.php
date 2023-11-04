6e8c5811a0dfedbf9fbe88f548ef883cda7dc14e
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2023-11-04 22:02:29 +0000

    correction game - games

--- src/Cli.php
+++ src/Cli.php
@@ -17,6 +17,6 @@ namespace Hexlet\Code\Cli;
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
+line('Welcome to the Brain Games!');
 $name = prompt('May I have your name?');
 line("Hello, %s!", $name);
