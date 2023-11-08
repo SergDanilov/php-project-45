3bb27c938674f0fcfa400b642e6ce1dd8d5e1efc
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2023-11-08 18:00:55 +0000

    modified some part of logic work

--- src/engine.php
+++ src/engine.php
@@ -1,49 +1,27 @@
 <?php
 /**
- * Greeting and getting to know the game user
+ * Engine it is a same part of each game
  * php version 8.1.24
  *
- * This allows greeting with User and know his name.
- * Then calculation random expression and asking user answer. 
- * After that comparing user's answer with correct answer. 
+ * This allows use same code in each game 
  *
  * @category  LearnProject
  * @package   Phpproject
  * @author    Sergey Danilov <danilovserg1985s@gmail.com>
  * @copyright 2023 Sergey Danilov
  * @license   no Licence
- * @link      https://github.com/SergDanilov/php-project-45/blob/main/src/brainCalculation.php.php
+ * @link      https://github.com/SergDanilov/php-project-45/blob/main/src/engine.php
  */
-namespace Hexlet\Code\brainCalculation;
+namespace Hexlet\Code\engine;
 
-use function cli\line;
-use function cli\prompt;
-
-line("Welcome to the Brain Games!");
-$name = prompt("May I have your name?");
-line("Hello, %s!", $name);
-line("Answer 'yes' if the number is even, otherwise answer 'no'.");
-$win = 0;
-for ($i = 0; $i < 3;) {
-    $number = rand(1, 1000);
-    line('Question: ' . $number);
-    $answer = prompt("Your answer");
-    if ($number % 2 === 0) {
-        $correctAnswer = "yes";
-    } else {
-        $correctAnswer = "no";
-    }
-    if ($answer === $correctAnswer) {
-        line("Correct!");
-        $i++;
-        $win++;
-    } else {
+if ($answer === $correctAnswer) {
+    line("Correct!");
+    $i++;
+    $win++;
+} else {
         line("'{$answer}' is wrong answer ;(.");
         line("Correct answer was '{$correctAnswer}'.");
         line("Let's try again, " . $name . "!");
         break;
-    }
 }
-if ($win == 3) {
-    line("Congratulations {$name}!");
-}
+
