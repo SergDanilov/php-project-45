0ce4a150d7ce12a15fd19ca9b76de138356646fa
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2023-11-11 19:55:02 +0000

    made logic for game

--- src/brainCalculation.php
+++ src/brainCalculation.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * Greeting and getting to know the game user
- * php version 8.1.24
- *
  * This allows greeting with User and know his name.
  * Then calculation random expression and asking user answer. 
- * After that comparing user's answer with correct answer. 
+ * After that comparing user's answer with correct answer.
+ * php version 8.1.24
+ * 
  *
  * @category  LearnProject
  * @package   Phpproject
@@ -16,34 +15,56 @@
  */
 namespace Hexlet\Code\brainCalculation;
 
+use Hexlet\Code\greeting;
+use Hexlet\Code\engine;
+
 use function cli\line;
 use function cli\prompt;
 
-line("Welcome to the Brain Games!");
-$name = prompt("May I have your name?");
-line("Hello, %s!", $name);
-line("Answer 'yes' if the number is even, otherwise answer 'no'.");
+$greeting_part = __DIR__ . '/../src/greeting.php';
+$engine_part = __DIR__ . '/../src/engine.php';
+$homePath = __DIR__ . '/../src/brainCalculation.php';
+
+global $gameOver;
+global $homePath;
+
+//greeting part
+require_once $greeting_part;
+
+//main part game
+line("What is the result of the expression?");
 $win = 0;
+$gameOver = false;
 for ($i = 0; $i < 3;) {
-    $number = rand(1, 1000);
-    line('Question: ' . $number);
-    $answer = prompt("Your answer");
-    if ($number % 2 === 0) {
-        $correctAnswer = "yes";
-    } else {
-        $correctAnswer = "no";
+    
+    $numberOne = rand(0, 100);
+    $numberTwo = rand(0, 25);
+
+    $operatorArray = ["+","-","*"];
+    $indexRandom = rand(0, 2);
+    $operator = $operatorArray[$indexRandom];
+    switch ($operator) {
+        case "+":
+            $correctAnswer = $numberOne + $numberTwo;
+            break;
+        case "-":
+            $correctAnswer = $numberOne - $numberTwo;
+            break;
+        case "*":
+            $correctAnswer = $numberOne * $numberTwo;
+            break;
     }
-    if ($answer === $correctAnswer) {
-        line("Correct!");
-        $i++;
-        $win++;
-    } else {
-        line("'{$answer}' is wrong answer ;(.");
-        line("Correct answer was '{$correctAnswer}'.");
-        line("Let's try again, " . $name . "!");
+    
+    line("Question: {$numberOne} {$operator} {$numberTwo}");
+    $answer = (int)prompt("Your answer");
+    var_dump($answer);
+
+    require $engine_part;
+    if ($gameOver) {
         break;
     }
 }
+//winner
 if ($win == 3) {
-    line("Congratulations {$name}!");
+    line("Congratulations, {$name}!");
 }
