d0047375c94bce0f31b04d108240f698dda462ee
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2023-12-02 11:04:09 +0000

    fixed logic game, removed repeating parts in file

---
+++ src/Games/BrainCalc.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This calculation random expression and asking user answer.
+ * After that comparing user's answer with correct answer.
+ * php version 8.1.24
+ *
+ * @category  LearnProject
+ * @package   Phpproject
+ * @author    Sergey Danilov <danilovserg1985s@gmail.com>
+ * @copyright 2023 Sergey Danilov
+ * @license   no Licence
+ * @link      https://github.com/SergDanilov/php-project-45/blob/main/src/Games/brainCalc.php
+ */
+
+namespace Braingames\Games\BrainCalc;
+
+use function Braingames\Engine\runGameEngine;
+use function cli\line;
+use function cli\prompt;
+
+use const Braingames\Engine\ROUNDS_COUNT;
+use const Braingames\Engine\RANDOM_START_NUM;
+use const Braingames\Engine\RANDOM_END_NUM;
+
+const TASK = "What is the result of the expression?";
+
+function runBrainCalc()
+{
+    //main part game
+    $questions = [];
+    $correctAnswers = [];
+
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+        $numberOne = rand(RANDOM_START_NUM, RANDOM_END_NUM);
+        $numberTwo = rand(RANDOM_START_NUM, RANDOM_END_NUM);
+        $operatorArray = ["+","-","*"];
+        $indexRandom = rand(0, 2);
+
+        $operator = $operatorArray[$indexRandom];
+        switch ($operator) {
+            case "+":
+                $correctAnswers[$i] = $numberOne + $numberTwo;
+                break;
+            case "-":
+                $correctAnswers[$i] = $numberOne - $numberTwo;
+                break;
+            case "*":
+                $correctAnswers[$i] = $numberOne * $numberTwo;
+                break;
+        }
+
+        $questions[$i] = "{$numberOne} {$operator} {$numberTwo}";
+    }
+    runGameEngine($questions, $correctAnswers, TASK);
+}
