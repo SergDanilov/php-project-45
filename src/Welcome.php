9814c1ddcfedcb9ca5fb38b46b7d4c21b526f2cf
Sergey Dan <60783739+SergDanilov@users.noreply.github.com>
2023-12-04 08:55:19 +0000

    removed comment

--- src/Welcome.php
+++ src/Welcome.php
@@ -19,7 +19,6 @@ use function cli\prompt;
 
 function runWelcome()
 {
-    //greeting
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
